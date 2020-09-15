f318fcce54f91f70423591df5e02e2d74db66df7
Riccardo Colombo <rikyregna@gmail.com>
2020-09-15 15:51:37 +0200

    finish

--- index.php
+++ index.php
@@ -2,5 +2,38 @@
 Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
 
 <?php 
-	echo('ciao');
+	$testo = "Nel mezzo del cammin di nostra vita mi ritrovai per una selva oscura ché la diritta via era smarrita.";
+
+	// $badword = 'belva';
+
+	$badword = $_GET['text'];
+
+	$newTesto = str_replace($badword, '***', $testo, $count);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Badwords find</title>
+</head>
+<body>
+<h1>Badwords find with PHP</h1>
+<p> Il testo da correggere è : 
+<?php 
+	echo " $testo";
+?>
+</p>
+<p>Il testo corretto è : 
+<?php
+	echo " $newTesto";
+?>
+</p><span>Caratteri sostituiti : 
+<?php 
+	echo " $count"
+?></span>
+	
+</body>
+</html>
