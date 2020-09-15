4c33bba9190776fd942b0db5f5472f8d030498c4
Riccardo Colombo <rikyregna@gmail.com>
2020-09-15 15:33:02 +0200

    init

---
+++ index.php
@@ -0,0 +1,6 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
+
+<?php 
+	echo('ciao');
+?>
